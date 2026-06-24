2683b85f30af39ad761eba10f7ddb80077034df9
Raggy-Sharma <raghava.sharma2892@gmail.com>
2026-06-24 12:14:48 +0530

    FIxed player faces not rendering for few players. Added code to render Bench players

--- askFootball_frontend_React/src/features/Player/PlayerFace.tsx
+++ askFootball_frontend_React/src/features/Player/PlayerFace.tsx
@@ -1,11 +1,15 @@
 const PLAYER_FACE_CDN_BASE_URL = import.meta.env.VITE_SOFIFA_BASE_URL
-export function PlayerFace({ playerId }) {
-    const playerIdSplit1 = String(playerId).slice(0, 3)
-    const playerIdSplit2 = String(playerId).slice(3, 6)
-    const playerFaceUrl = `${PLAYER_FACE_CDN_BASE_URL}${playerIdSplit1}/${playerIdSplit2}/26_90.png`
+const VITE_PLAYER_FACE_VERSION_AND_DIMENSION = import.meta.env.VITE_PLAYER_FACE_VERSION_AND_DIMENSION
+const PlayerFace = ({ playerId }) => {
+    const append0: boolean = String(playerId).length < 6;
+    const playerIdSplit1 = append0 ? `0${String(playerId).slice(0, 2)}` : String(playerId).slice(0, 3)
+    const playerIdSplit2 = append0 ? String(playerId).slice(2, 6) : String(playerId).slice(3, 6)
+    const playerFaceUrl = `${PLAYER_FACE_CDN_BASE_URL}${playerIdSplit1}/${playerIdSplit2}/${VITE_PLAYER_FACE_VERSION_AND_DIMENSION}`
     return (
-        <div className="p-4 m-4">
+        <div className="p-4 m-2">
             <img src={playerFaceUrl} alt={"Alt"} referrerPolicy="no-referrer" />
         </div>
     )
 }
+
+export default PlayerFace;
--- askFootball_frontend_React/src/features/Squad/Bench.tsx
+++ askFootball_frontend_React/src/features/Squad/Bench.tsx
@@ -1,7 +1,21 @@
-export function Bench() {
+import useStore from '@/Store';
+import PlayerFace from '@/features/Player/PlayerFace';
+const Bench = () => {
+    const selectedClubBench = useStore((state) => state.selectedClubBench);
     return (
         <div>
-            <h1>Bench</h1>
+            <h1>Substitues and Reserves</h1>
+            <div className="grid grid-cols-2 gap-2 overflow-y-auto">
+                {selectedClubBench?.map((player) => (
+                    <div key={player.player_id} className="flex flex-col items-center justify-center">
+                        <PlayerFace playerId={player.player_id} />
+                        <span className="text-xs font-medium">{player.display_name}</span>
+                        <span className="text-[10px] uppercase opacity-70">{player.club_position}</span>
+                    </div>
+                ))}
+            </div>
         </div>
     )
 }
+
+export default Bench;
